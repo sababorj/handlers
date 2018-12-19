d9624e3bded1c9898780db8600e6ae6cd9a0afd5
Saba Schock <sababorjsaba@gmail.com>
2018-12-18 17:00:15 -0800

    get logic

--- schema.sql
+++ schema.sql
@@ -1,15 +1,15 @@
-CREATE DATABASE moviePlannerDB;
-USE moviePlannerDB;
+CREATE DATABASE wish_saver_db;
+USE wish_saver_db;
 
--- Create the table plans.
-CREATE TABLE movies
+-- Create the table tasks.
+CREATE TABLE wishes
 (
 id int NOT NULL AUTO_INCREMENT,
-movie varchar(255) NOT NULL,
+wish varchar(255) NOT NULL,
 PRIMARY KEY (id)
 );
 
 -- Insert a set of records.
-INSERT INTO movies (movie) VALUES ('Plan to fight a ninja.');
-
-SELECT * FROM movies;
+INSERT INTO wishes (wish) VALUES ('go on a trip');
+INSERT INTO wishes (wish) VALUES ('have a job');
+INSERT INTO wishes (wish) VALUES ('be a developer');
--- servers.js
+++ servers.js
@@ -1,39 +1,42 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 
-port = process.env.PORT || 8080;
+var exphdb = require('express-handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended : true }));
+app.use(express.urlencoded({ extende : true}));
 
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout : "main" }));
+app.engine("handlebars", exphdb( { defaultLayout :"main"}));
 app.set("view engine", "handlebars");
 
-var mysql = require('mysql');
+var mysql = require("mysql");
 
-var connection = mysql.createConnection( {
+var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user : "root",
-    password : "password",
-    database: "moviePlannerDB"
-})
+    user: "root",
+    password: "password",
+    database: "wish_saver_db"
+});
 
 connection.connect( (err) => {
     if (err) throw err
-    console.log("connected to the database")
+    console.log("connected to the db")
 })
 
-app.get("/", (req,res) => {
+app.get("/", (req, res)=> {
+    connection.query("SELECT * FROM wishes;", (err, data) => {
+        if (err) throw err
+        //res.json(data);
+        res.render("home", {
+            wishes: data
+        });
+    });
+});
 
-})
 
-app.post("/", (req,res) => {
-    
-})
 
 app.listen(port, ()=> {
-    console.log("I am listening")
-})
+    console.log("I am listening to you dude")
+});
